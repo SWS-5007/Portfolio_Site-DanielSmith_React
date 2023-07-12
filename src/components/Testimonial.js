a9edc774ef460647895efae5781238c818aac2ca
Daniel Smith <dalejandrole@gmail.com>
2023-07-12 10:59:15 -0300

    beta version updated...

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -9,38 +9,38 @@ function Testimonial() {
                 <div className="row">
                     <div className="col-lg-4">
                         <div className="team-member">
-                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/1.jpg" alt="..." />
-                            <h4>Parveen Anand</h4>
+                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/Alex Backer.jpg" alt="..." />
+                            <h4>Alex Backer</h4>
                             <p className="text-muted">
                                 “Daniel was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.”
                             </p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i className="fab fa-twitter"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i className="fab fa-facebook-f"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Alex Backer Twitter Profile"><i className="fab fa-twitter"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Alex Backer Facebook Profile"><i className="fab fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Alex Backer LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="team-member">
-                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/2.jpg" alt="..." />
-                            <h4>Diana Petersen</h4>
+                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/Antonio Piazzolla.jpg" alt="..." />
+                            <h4>Antonio Piazzolla</h4>
                             <p className="text-muted">
                                 “Daniel's a clear communicator with the tenacity and confidence to really dig in to tricky design scenarios and the collaborative friction that's needed to produce excellent work.”
                             </p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i className="fab fa-twitter"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i className="fab fa-facebook-f"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Antonio Piazzolla Twitter Profile"><i className="fab fa-twitter"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Antonio Piazzolla Facebook Profile"><i className="fab fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Antonio Piazzolla LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="team-member">
-                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/3.jpg" alt="..." />
-                            <h4>Larry Parker</h4>
+                            <img className="mx-auto rounded-circle" src="assets/img/testimonials/Michael Coudrey.jpg" alt="..." />
+                            <h4>Michael Coudrey</h4>
                             <p className="text-muted">
                             “Daniel was a very trusted and talented DevOps Engineer. During with this project I've known about him very well and I wish to work with him again.”
                             </p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i className="fab fa-twitter"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i className="fab fa-facebook-f"></i></a>
-                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Michael Coudrey Twitter Profile"><i className="fab fa-twitter"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Michael Coudrey Facebook Profile"><i className="fab fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Michael Coudrey LinkedIn Profile"><i className="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
